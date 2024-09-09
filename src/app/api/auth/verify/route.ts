566ba206dfad5960d0c588bff0910dfd63ebfe6c
muskan <muskanloach984@gmail.com>
2024-09-09 14:15:17 +0530

    verify api

--- src/app/api/auth/verify/route.ts
+++ src/app/api/auth/verify/route.ts
@@ -1,4 +1,4 @@
-// src/app/api/auth/verify/route.ts
+
 import jwt from 'jsonwebtoken';
 import { NextRequest, NextResponse } from 'next/server';
 
@@ -15,10 +15,9 @@ export async function POST(request: NextRequest) {
       );
     }
 
-    // Decode the token and check verification status
+   
     const decoded = jwt.verify(token, jwtSecret) as { isVerified: boolean; email: string };
 
-    // Respond with authentication status and user details
     return NextResponse.json(
       { isAuthenticated: true, isVerified: decoded.isVerified, email: decoded.email },
       { status: 200 }
