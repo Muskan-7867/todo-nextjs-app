f5c24ace3e1109763296baad6fdd363aa1cf2812
muskan <muskanloach984@gmail.com>
2024-09-09 23:19:24 +0530

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,21 @@
-"use client"
+"use client";
 import React from "react";
-import Link from 'next/link'
-import {  useSearchParams } from "next/navigation";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
 
 const Navbar = () => {
-  const searchParams : ReadonlyURLSearchParams = useSearchParams();
-  const todosData: string | null  = searchParams.get("todos");
-  
+  const searchParams: ReadonlyURLSearchParams = useSearchParams();
+  const todosData: string | null = searchParams.get("todos");
+
   return (
-    <nav>
-      <Link href="/" className={todosData=== null ? "active" : ""}>
+    <nav className="flex space-x-4 mt-8   ">
+      <Link   href="/" className={`${!todosData ? 'font-bold' : ''}`}>
         All
       </Link>
-      <Link href="/?todos=active" className={todosData === "active" ? "active" : ""}>
+      <Link href="/?todos=active" className={`${todosData === 'active' ? 'font-bold' : ''}`}>
         Active
       </Link>
-      <Link href="/?todos=completed" className={todosData=== "completed" ? "active" : ""}>
+      <Link href="/?todos=completed" className={`${todosData === 'completed' ? 'font-bold' : ''}`}>
         Completed
       </Link>
     </nav>
