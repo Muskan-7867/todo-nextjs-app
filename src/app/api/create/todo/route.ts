d346f7e8d642cb88feee001b34ea14e31821f4dc
muskan <muskanloach984@gmail.com>
2024-09-12 22:00:57 +0530

    create todo api

--- src/app/api/create/todo/route.ts
+++ src/app/api/create/todo/route.ts
@@ -1,16 +1,16 @@
-import { NextRequest, NextResponse } from "next/server";
-import Todo from "src/models/todomodel";
-import { connect } from "src/utills/db";
 
-// Establish a database connection
+import { NextRequest, NextResponse } from 'next/server';
+import Todo from 'src/models/todomodel';
+import { connect } from 'src/utills/db';
+
+
 connect();
 
 export async function POST(request: NextRequest) {
   try {
     const reqBody = await request.json();
-    const { task, targetTime = null, status = 'pending' } = reqBody; 
+    const { task, status = 'pending' } = reqBody; 
 
-    // Basic validation
     if (!task) {
       return NextResponse.json(
         { error: 'Missing required fields', success: false },
@@ -18,17 +18,15 @@ export async function POST(request: NextRequest) {
       );
     }
 
-    // Create a new Todo instance
+  
     const newTodo = new Todo({
       task,
       status,
-      targetTime, // This will be null if not provided
     });
 
-    // Save the Todo to the database
     const savedTodo = await newTodo.save();
 
-    // Return a success response
+   
     return NextResponse.json(
       {
         message: 'Todo created successfully',
@@ -38,7 +36,7 @@ export async function POST(request: NextRequest) {
       { status: 200 }
     );
   } catch (error: any) {
-    // Return an error response
+   
     return NextResponse.json(
       {
         error: error.message || 'Internal server error',
@@ -48,4 +46,3 @@ export async function POST(request: NextRequest) {
     );
   }
 }
-
