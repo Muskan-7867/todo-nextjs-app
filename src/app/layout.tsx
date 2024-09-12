4397bf8b2b407e06609fba090b18f65108aa597f
muskan <muskanloach984@gmail.com>
2024-09-12 22:01:21 +0530

    layout pg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import "./globals.css";
-import Navbar from "src/components/Navbarmain";
+
+import Navbarmain from "src/components/Navbarmain";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
+        <Navbarmain/>
 
         {children}
       </body>
