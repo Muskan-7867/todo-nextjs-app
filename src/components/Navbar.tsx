4e88b2ab1fbc6e0110dd6abf1a9533f8bb90fc66
muskan <muskanloach984@gmail.com>
2024-09-12 21:58:37 +0530

    todo navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,14 +8,14 @@ const Navbar = () => {
   const todosData: string | null = searchParams.get("todos");
 
   return (
-    <nav className="flex space-x-4 mt-8   ">
-      <Link   href="/" className={`${!todosData ? 'font-bold' : ''}`}>
+    <nav className="flex flex-col md:flex-row md:space-x-4 mt-8 px-4 md:px-8 py-2 bg-white shadow-md">
+      <Link href="/" className={`block md:inline ${!todosData ? 'font-bold' : ''} py-2 md:py-0`}>
         All
       </Link>
-      <Link href="/?todos=active" className={`${todosData === 'active' ? 'font-bold' : ''}`}>
+      <Link href="/?todos=active" className={`block md:inline ${todosData === 'active' ? 'font-bold' : ''} py-2 md:py-0`}>
         Active
       </Link>
-      <Link href="/?todos=completed" className={`${todosData === 'completed' ? 'font-bold' : ''}`}>
+      <Link href="/?todos=completed" className={`block md:inline ${todosData === 'completed' ? 'font-bold' : ''} py-2 md:py-0`}>
         Completed
       </Link>
     </nav>
