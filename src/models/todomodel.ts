ac6d4754d240daab16a4c5278786b86998d4539d
muskan <muskanloach984@gmail.com>
2024-09-12 23:42:57 +0530

    todomodel

--- src/models/todomodel.ts
+++ src/models/todomodel.ts
@@ -6,6 +6,10 @@ export interface ITodo extends Document {
   task: string;
   status: 'inprogress' | 'pending' | 'completed';
   createdAt: Date;
+<<<<<<< HEAD
+=======
+  targetTime?: Date; // Marking targetTime as optional in the interface
+>>>>>>> master
   user: mongoose.Schema.Types.ObjectId;
 }
 
@@ -25,13 +29,25 @@ const TodoSchema: MongooseSchema = new MongooseSchema(
     createdAt: {
       type: Date,
       default: Date.now,
+<<<<<<< HEAD
+=======
+    },
+    targetTime: {
+      type: Date,
+      required: false, // Ensure this field is optional
+>>>>>>> master
     },
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
+<<<<<<< HEAD
     }
     
   },
+=======
+    },
+  }
+>>>>>>> master
 );
 
 // Use existing model if available or create a new one
