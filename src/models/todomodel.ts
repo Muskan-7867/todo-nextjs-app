46d3bb920eb82ab5878f2efd0fc766b092642fb9
muskan <muskanloach984@gmail.com>
2024-09-12 21:59:26 +0530

    todo model

--- src/models/todomodel.ts
+++ src/models/todomodel.ts
@@ -1,47 +1,40 @@
+
 import mongoose, { Document, Schema as MongooseSchema } from 'mongoose';
 
-// Define the TypeScript interface representing a Todo document
+
 export interface ITodo extends Document {
   task: string;
   status: 'inprogress' | 'pending' | 'completed';
   createdAt: Date;
-  targetTime: Date;
   user: mongoose.Schema.Types.ObjectId;
 }
 
-// Create the Mongoose schema based on the interface
+
 const TodoSchema: MongooseSchema = new MongooseSchema(
   {
     task: {
       type: String,
       required: true,
-      trim: true
+      trim: true,
     },
     status: {
       type: String,
       enum: ['inprogress', 'pending', 'completed'],
-      default: 'pending'
+      default: 'pending',
     },
     createdAt: {
       type: Date,
-      default: Date.now
-    },
-    targetTime: {
-      type: Date,
-      required: true
+      default: Date.now,
     },
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
-     
     }
+    
   },
-  {
-    timestamps: true
-  }
 );
 
+// Use existing model if available or create a new one
 const Todo = mongoose.models.Todo || mongoose.model<ITodo>('Todo', TodoSchema);
-
 
 export default Todo;
